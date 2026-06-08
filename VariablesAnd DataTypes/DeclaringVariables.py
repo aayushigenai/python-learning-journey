fc08d767eb97b043ef7301636b9a0f5cd78bc4e8
Armor-YUSHI <aayushibhatt023@gmail.com>
2026-06-08 16:10:16 +0530

    Add Python learning files

---
+++ VariablesAnd DataTypes/DeclaringVariables.py
@@ -0,0 +1,6 @@
+name= "Aayushi"
+age=20
+price=25.00
+
+print("my name is :",name)
+print("my age is :",age)
---
+++ first PRgorame/firstprogram.py
@@ -0,0 +1,3 @@
+print("My name is aayushi bhatt.","I am 18 years old.")
+print(23)
+print(23+23)
