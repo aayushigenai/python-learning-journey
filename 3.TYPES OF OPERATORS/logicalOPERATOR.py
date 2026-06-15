74fe07efd2b6b086255a196412af01f3b979cae6
Armor-YUSHI <aayushibhatt023@gmail.com>
2026-06-15 20:18:40 +0530

    practiced logical operator

---
+++ 3.TYPES OF OPERATORS/logicalOPERATOR.py
@@ -0,0 +1,29 @@
+#not operator
+a=20
+b=30
+input=not True
+print(input)
+print(not(a>b))
+
+#and operator
+
+a= True
+b= False
+print("AND operator :", a and b)
+
+c= True
+d= True
+print("AND operator :", a and b)
+# when both values are true then only answer is true.
+
+#or operator
+
+x=True
+y=False
+print("OR operator :", a or b)
+
+#turns out to be true even if one value is true.
+
+p=False
+q=False
+print("OR operator :", p or q)
