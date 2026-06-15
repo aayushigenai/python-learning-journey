ab137e0e049f11ae9f876db94935fab69919c9fd
Armor-YUSHI <aayushibhatt023@gmail.com>
2026-06-15 18:36:10 +0530

    Add data types examples

---
+++ VariablesAnd DataTypes/DataTypes.py
@@ -0,0 +1,12 @@
+age=25
+salary=25000.00
+name="aayushi"
+alive=True
+hobbies=None 
+
+print("all the data types in python")
+print(type(age))
+print(type(salary))
+print(type(name))
+print(type(alive))
+print(type(hobbies))
