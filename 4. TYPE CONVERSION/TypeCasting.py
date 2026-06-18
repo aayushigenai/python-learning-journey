f7044b462f26c7d2c7340f401b938cb09be45f4d
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-18 15:25:28 +0530

    type casting (manually)

---
+++ 4. TYPE CONVERSION/TypeCasting.py
@@ -0,0 +1,11 @@
+a="2"
+b=22.125
+#sum(a+b)
+#print(sum)     # will genrate error asa string can not be added into a float.so type casting of variable a is required.
+
+#converting string variable a into integer using the concept of type casting.
+
+a=int("2")   #--> converted into string
+b=22.125
+sum=a+b     #--->integer+float=float
+print(sum)
