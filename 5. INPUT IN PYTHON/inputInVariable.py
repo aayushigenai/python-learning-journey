c7b12837cb40681d52b95b7f974a98dc1fb148d0
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-18 16:15:30 +0530

    variable in python

---
+++ 5. INPUT IN PYTHON/inputInVariable.py
@@ -0,0 +1,11 @@
+name=input("what is your name:")
+print("name is :", name)
+print(type(name)) #result of input statement is always a string in python.
+
+#wanting a int answer insdet of string , can use type casting over here.
+
+age = int(input("what is your age:"))
+print("age is :", age)
+print(type(age)) # result is integer.
+
+
