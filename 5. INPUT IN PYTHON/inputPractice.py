b12f141fb77376adc892768005843cb52f95000f
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-18 16:16:20 +0530

    input taking practice using type casting

---
+++ 5. INPUT IN PYTHON/inputPractice.py
@@ -0,0 +1,7 @@
+name=input("enetr your name:")
+age=int(input("enter your age:"))
+marks=int(input("enetr your marks:"))
+
+print("what is your name :",name)
+print("what is your age :",age)
+print("what is your marks:",marks)
