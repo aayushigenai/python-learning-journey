aea61a54aa06e441cb817b0f0582d51e615a4f9b
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-18 16:18:06 +0530

    question3

---
+++ 5. INPUT IN PYTHON/practiceQuestion3.py
@@ -0,0 +1,6 @@
+#write a programe to input two floating point numbers  and print their averages.
+
+first=float(input("enter the first floating point number:"))
+second=float(input("enter the second floating point number:"))
+average=(first+second)/2
+print("average of two floating point numbers is:",average)
