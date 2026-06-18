224fc634ca675191a5454fc3c9665f111e5a0c5b
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-18 16:18:15 +0530

    question4

---
+++ 5. INPUT IN PYTHON/practiceQuestion4.py
@@ -0,0 +1,8 @@
+#write a programe to input 2 int numbers , a and b.
+#  print true if  a is  greater than or equal to b.if not print false.
+
+
+a=int(input("enter the integer a:"))
+b=int(input("enter the integer b:"))
+
+print(a>=b)
