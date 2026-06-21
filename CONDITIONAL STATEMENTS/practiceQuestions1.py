740e13fafba0f2e132b3e4f5fc7bc2200deba113
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-21 12:02:22 +0530

    practice question 1

---
+++ CONDITIONAL STATEMENTS/practiceQuestions1.py
@@ -0,0 +1,10 @@
+#write a program to check if a number entered by user is odd or even.
+
+number=int(input("enter the number :"))
+
+if(number%2==0 ):
+    print("even number")
+else:
+    print("odd number")
+
+    
