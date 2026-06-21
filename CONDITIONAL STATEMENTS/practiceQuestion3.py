21abaef8591a576434611fe5d9b4ebb5b2acf30d
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-21 12:02:35 +0530

    practice question 3

---
+++ CONDITIONAL STATEMENTS/practiceQuestion3.py
@@ -0,0 +1,8 @@
+#write a programe to check if a number is a multiple of 7 or not.
+
+num=int(input("enter the number :"))
+
+if(num%7==0):
+    print("number is multiple of seven.")
+else:
+    print("number is not multiple of seven.")    
