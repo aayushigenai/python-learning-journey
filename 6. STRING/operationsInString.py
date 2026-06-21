5ed1353f44bded8e43999871317c35d3a9d5c87b
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-21 10:46:35 +0530

    string operations

---
+++ 6. STRING/operationsInString.py
@@ -0,0 +1,20 @@
+#concatenation
+str1="hello"
+str2="aayushi"
+print(str1+str2)
+
+#length of a string
+
+ans1=len(str1+str2)
+print(ans1)
+
+#indexing
+str4="YUSHI BHATT"
+print(str4[0])
+print(str4[1])
+
+
+#slicing
+str5="GAYATRI"
+print(str5[1:4])
+print(str5[1:len(str5)])
