ba65804f259703839d1a32555d2a5f2f2deaef78
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-21 12:01:03 +0530

    if-elif-elseStatements

---
+++ CONDITIONAL STATEMENTS/if-ilif-elseStatement.py
@@ -0,0 +1,11 @@
+light="green"
+if(light=="yellow"):
+    print("wait")
+elif(light=="green"):
+    print("go")
+elif(light=="red"):
+    print("stop")
+else:
+    print("light is broken")    
+
+print("end of code")            
