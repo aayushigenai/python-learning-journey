647ff31d00c9bc273bb2d7e965c453d22c87d7d7
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-21 10:47:32 +0530

    practice questions on string

---
+++ 6. STRING/practiceQuestion.py
@@ -0,0 +1,11 @@
+#write a programe to input user's first name and print its length.
+
+name=input("what is your first name :")
+print("first name:",name)
+
+print(len(name))
+
+#write a programe to find occurance of "$" in a string.
+
+str="my name is $$$$$$$ and surname is $$$$"
+print(str.count("$"))
