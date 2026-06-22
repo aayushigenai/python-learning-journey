df1b7e7bc1e42b270777ae7879cee41ea6f14870
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-22 07:27:54 +0530

    declaring lists

---
+++ LISTS/declration.py
@@ -0,0 +1,9 @@
+marks=[23.5,35.5,75.5,85.5,45.5]
+print(marks[0])
+print(marks[1])
+print(type(marks))
+print(len(marks))
+
+#declaring elemenets of multiple data type in the same list.
+student=["Aayushi",35,"Delhi"]
+print(student)
