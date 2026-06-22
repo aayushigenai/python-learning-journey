18016ca1fffb72bc905edaefc0d8d942eac6b6c1
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-22 16:18:41 +0530

    nested dictionaries

---
+++ 10.DICTIONARY/nested dictionaries.py
@@ -0,0 +1,14 @@
+dic={
+    "name":"Aayushi",
+    "surname":"Bhatt",
+    "age":20,
+    "subjects":{
+        "maths":35,
+        "chemistry":55,
+        "physics":75,
+        "computer science":85,
+    },
+    "isAdult":True,
+}
+print(dic)
+print(dic["subjects"])
