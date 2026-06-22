b0f529dbd1c8254d901f086d3863aa5fc34f2cc5
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-22 08:55:10 +0530

    practice question 3

---
+++ TUPLES/practiceQuestion3.py
@@ -0,0 +1,6 @@
+#write a program to count the number of students with the "A" grade in the following tuple.
+#["C","D","A","A","B","B","A"]
+
+tuple=["C","D","A","A","B","B","A"]
+tup=tuple.count("A")
+print("number of students with A grade:",tup)
