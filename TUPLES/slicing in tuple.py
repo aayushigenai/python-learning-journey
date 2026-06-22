c7c3dc4898857206f813a65eabf81f3a4041bcff
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-22 08:54:42 +0530

    slicing in tuples

---
+++ TUPLES/slicing in tuple.py
@@ -0,0 +1,5 @@
+tuple=(23,33,12,41,23,23,83)
+print(tuple[1:3])
+
+print(tuple.index(23))    #returns index of first occurence of the element.
+print(tuple.count(23))    #returns the count of the occurence of the element.
