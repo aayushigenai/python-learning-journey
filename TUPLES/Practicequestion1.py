c57197fcf6dbe5ec33bc450be0ea6c770cfab6be
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-22 08:54:52 +0530

    practice question 1

---
+++ TUPLES/Practicequestion1.py
@@ -0,0 +1,11 @@
+#write a program to aslk the user,to enetr the names,pf their fav three movies and store them in list.
+
+movies=[]
+mov1=input("enter the name of the first movie :")
+mov2=input("enter the name of the second movie:")
+mov3=input("enter the name of the third movie: ")
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+print(movies)
