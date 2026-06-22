c0fcc3ebbf1fa917e0f3482f8307af554d30c1b8
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-22 16:18:31 +0530

    declaring a dictionary

---
+++ 10.DICTIONARY/declaringAaDictionary.py
@@ -0,0 +1,20 @@
+dic={
+
+    "name" : "Aayushi",
+    "age" : 34,
+    "course": "CSE",
+    "isAdult":True,
+
+}
+print(dic)
+
+#key cannot be list,tuple, but value can be list and tuple.
+info={
+    "name":"Aayushi",
+    "subjects": ["os","dbms","programming","coding"],
+    "topics":("python","array","linklist"),
+    "age":20,
+}
+print(info)
+
+print(type(dic))
