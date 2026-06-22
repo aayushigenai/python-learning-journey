7ef46f581a777ff7b68fbd0e606112936a42a827
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-22 18:00:32 +0530

    question 2

---
+++ 11.SETS/practiceQuestion2.py
@@ -0,0 +1,7 @@
+#You are given a list of subjects for students. Assume one classroom is required for one subject. How many classrooms are needed by all students?
+#Subjects list:
+#"python", "java", "C++", "python", "javascript","java", "python", "java", "C++", "C"
+
+subjects={"python", "java", "C++", "python", "javascript","java", "python", "java", "C++", "C"}
+
+print(len(subjects))
