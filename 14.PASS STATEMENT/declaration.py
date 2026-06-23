7f8e93745d0f0790ef297dab1c280253aed5ff57
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-23 16:45:42 +0530

    declration of pass statement

---
+++ 14.PASS STATEMENT/declaration.py
@@ -0,0 +1,6 @@
+for el in range(1,10):
+    pass
+print("work is  done")
+
+#here pass statemnet was used to skip the work inside the loop. and moving further.
+#that loop can be used for future use.
