6dce26dcc9da99c573ddb22281d54979e7ce5fcc
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-23 16:43:18 +0530

    practice question 3

---
+++ 12.LOOPS/practiceQuestion3.py
@@ -0,0 +1,10 @@
+#print the elements of the following list using a loop:
+#[1,4,9,16,25,36,49,64,81,100]
+
+list = [1,4,9,16,25,36,49,64,81,100]
+i=0
+while(i<len(list)):
+    print(list[i])
+    i+=1
+print("loop ended")    
+
