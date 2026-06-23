5c6072c835db5a5d23ef09f65939994bda21cb50
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-23 16:44:26 +0530

    range function

---
+++ 13.RANGE()/range function.py
@@ -0,0 +1,26 @@
+#range()
+#printing all the number in range.
+#seq=range(5)
+#print(range(5))
+#print(seq[0])
+#print(seq[1])
+#print(seq[2])
+#print(seq[3])
+#print(seq[4])
+#printing all the numbers in range using loop
+
+#for i in seq:
+#    print(i)
+
+
+#three ways of writing range
+range(5) # range(stop)
+range(2,5) # range(start,stop)
+range(2,5,2) #range(start, stop, step)
+
+
+#printing all the even numbers from 1 to 100.
+
+even=range(2,101,2)
+for i in even:
+    print(i)
