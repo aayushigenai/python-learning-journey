a1ce98273dceb744866b175de6a7f9c5dae0c220
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-24 22:31:19 +0530

    opening a file

---
+++ 17.INPUT-OUTPUT-IN -PYTHON/openingAFile.py
@@ -0,0 +1,11 @@
+f=open(r"C:\Users\Aayushi Bhatt\Desktop\python\17.INPUT-OUTPUT-IN -PYTHON\demo.txt","r")
+data=f.read()
+print(data)
+print(type(data))
+
+
+#for reading limited charachters
+f=open(r"C:\Users\Aayushi Bhatt\Desktop\python\17.INPUT-OUTPUT-IN -PYTHON\demo.txt","r")
+data=f.read(7)      #reading only 7 charcahters.
+print(data)
+print(type(data))
