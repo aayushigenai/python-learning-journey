e4b724c00403b3c55545ae989095c3d4c529094a
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-24 13:45:28 +0530

    practice question1

---
+++ 15.FUNCTIONS/practiceQuestion1.py
@@ -0,0 +1,8 @@
+#write a function to print the length of a list.(list is the parameter)
+
+def cal_len(list):
+    length=len(list)
+    print(length)
+    return length
+
+cal_len(list=[2,3,4,5,6,7,8])
