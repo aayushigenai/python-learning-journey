d7c782519c1efc9ea167f45fab7c444c3bce27a1
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-24 13:44:38 +0530

    buit-in-function

---
+++ 15.FUNCTIONS/Built-in-Function.py
@@ -0,0 +1,12 @@
+#functions that are already present in python language, 
+#example : print()
+#len()
+#range()
+#type()
+
+def calc_sum(x,y):
+    sum=x+y
+    return sum
+
+sum1= calc_sum(25,25)
+print(sum1)
