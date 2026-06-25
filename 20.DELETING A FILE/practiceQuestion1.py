d2f534af73b15cc71a66cc848bb82e31017e0542
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-25 18:34:00 +0530

    practice question 1

---
+++ 20.DELETING A FILE/practiceQuestion1.py
@@ -0,0 +1,8 @@
+# create a new file "practice.txt" using python. add the following data in it.
+# hi everyone
+# we are learning File I/O
+# using java
+# i like programming in java.
+
+with open("sample.txt","w") as f:
+    f.write("hi everyone \n we are learning File I/O \n using java \n i like programming in java.")
