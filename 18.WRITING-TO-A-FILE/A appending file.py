7c3cea96d11b6b1a5b776ed7b44fd293beb002b2
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-25 18:30:54 +0530

    appending  a file

---
+++ 18.WRITING-TO-A-FILE/A appending file.py
@@ -0,0 +1,6 @@
+#apends adds  the lines in the exsisting file.
+#adds to the file
+
+f=open(r"C:\Users\Aayushi Bhatt\Desktop\python\18.WRITING-TO-A-FILE\demo1.txt","a")
+f.write("\n i am every happy learning python right now. \n it is an amazing language.")
+f.close()
