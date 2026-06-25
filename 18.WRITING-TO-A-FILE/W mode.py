10e33e15ce55891e45ab83f4ad7c65d282c55458
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-25 18:31:37 +0530

    w mode in python

---
+++ 18.WRITING-TO-A-FILE/W mode.py
@@ -0,0 +1,6 @@
+#"w"---> overwrites everything written on the text file, and adds these new lines in that exsisting file.
+#overwrites the entire file.
+
+f=open(r"C:\Users\Aayushi Bhatt\Desktop\python\18.WRITING-TO-A-FILE\demo1.txt","w")
+f.write(" I am learning javascript right now. its code number is 12345.")
+f.close()
