734961f921255c1e0a62b90fd9e31cf9a90fb241
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-27 08:42:01 +0530

    encapsulation in python

---
+++ 21.OBJECT ORIENTED PROGRAMMING/encapsulation.py
@@ -0,0 +1,21 @@
+# capturing data and methods realted to each other in a capsule(class).
+
+class Student:
+
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+
+    def getAverage(self):
+        sum=0
+        for val in self.marks:
+            sum+=val
+        print("hey",self.name,"your average score of 3 subjects is :",sum/3)    
+
+
+
+s1=Student("aayushi",[80,80,90])
+s1.getAverage()
+
+s1.name="sumit"
+s1.getAverage()
