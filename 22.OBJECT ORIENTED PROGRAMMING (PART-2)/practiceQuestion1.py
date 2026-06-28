6d6dd17660a472b08a58c223a846c8bda7efde0a
Armor-YUSHI <aayushibhatt595@gmail.com>
2026-06-28 23:07:47 +0530

    practice question 1

---
+++ 22.OBJECT ORIENTED PROGRAMMING (PART-2)/practiceQuestion1.py
@@ -0,0 +1,19 @@
+#Define a circle class to create a circle with radius r using the constructor. 
+# Define an Area() method of the class which calculates the area of the circle.
+#define a perimeter() method of the class which allows you to  calculate teh perimeter of the circle.
+
+class Circle:
+
+    def __init__(self,radius):
+        self.radius=radius
+
+    def Area(self):
+        return (22/7) * self.radius **2
+
+    def perimeter(self):
+        return 2 * (22/7) * self.radius
+
+
+c1= Circle(21)
+print(c1.Area())
+print(c1.perimeter())        
